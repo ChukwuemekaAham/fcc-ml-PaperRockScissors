c18128b0eccf0aac33353b8c4c5a2d77e8c4c8c6
chukwuemeka <ahamchukwuemeka2@gmail.com>
2021-10-08 14:56:14 +0100

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,70 +1,43 @@
 import numpy as np
 import random
 
-ideal_response = {"P": "S", "R": "P", "S": "R"}
-my_moves = ["R"]
+order = {}
 opponent_history = []
-strategy = [0, 0, 0, 0]
-opponent_guess = ["", "", "", ""]
-strategy_guess = ["", "", "", ""]
-opponent_play_order = {}
-my_play_order = {}
 
 def player(prev_play):
-    if prev_play in ["R", "P", "S"]:
-        opponent_history.append(prev_play)
-        for i in range(0, 4):
-            if opponent_guess[i] == prev_play:
-                strategy[i] += 1
+  global opponent_history, order
+
+  n = 3
+
+  if prev_play in ["R","P","S"]:
+    opponent_history.append(prev_play)
+
+  # default 
+   
+  guess = "R"
+
+  if len(opponent_history) >= n:
+    input = "".join(opponent_history[-n:])
+
+    if "".join(opponent_history[-(n+1):]) in order.keys():
+      order["".join(opponent_history[-(n+1):])]+=1
     else:
-        reset()
+      order["".join(opponent_history[-(n+1):])]=1
 
-    my_last_ten = my_moves[-10:]
-    if len(my_last_ten) > 0:
-        my_most_frequent_move = max(set(my_last_ten), key=my_last_ten.count)
-        opponent_guess[0] = ideal_response[my_most_frequent_move]
-        strategy_guess[0] = ideal_response[opponent_guess[0]]
+    possible =[input + "R", input + "P", input + "S"]
 
-    if len(my_moves) > 0:
-        my_last_play = my_moves[-1]
-        opponent_guess[1] = ideal_response[my_last_play]
-        strategy_guess[1] = ideal_response[opponent_guess[1]]
+    for i in possible:
+      if not i in order.keys():
+        order[i] = 0
 
-    if len(opponent_history) >= 3:
-        opponent_guess[2] = predict_move(opponent_history, 3, opponent_play_order)
-        strategy_guess[2] = ideal_response[opponent_guess[2]]
+    predict = max(possible, key=lambda key: order[key])
 
-    if len(my_moves) >= 2:
-        opponent_guess[3] = ideal_response[predict_move(my_moves, 2, my_play_order)]
-        strategy_guess[3] = ideal_response[opponent_guess[3]]
-
-    best_strategy = np.argmax(strategy)
-    guess = strategy_guess[best_strategy]
-    if guess == "":
-        guess = "S"
-    my_moves.append(guess)
-    return guess
+    if predict[-1] == "P":
+      guess = "S"
+    if predict[-1] == "R":
+      guess = "P"
+    if predict[-1] == "S":
+      guess = "R"
 
 
-def predict_move(history, n, play_order):
-    if "".join(history[-n:]) in play_order.keys():
-        play_order["".join(history[-n:])] += 1
-    else:
-        play_order["".join(history[-n:])] = 1
-    possible = ["".join(history[-(n - 1) :]) + k for k in ["R", "P", "S"]]
-    for pm in possible:
-        if not pm in play_order.keys():
-            play_order[pm] = 0
-    predict = max(possible, key=lambda key: play_order[key])
-    return predict[-1]
-
-
-def reset():
-    global my_moves, opponent_history, strategy, opponent_guess, strategy_guess, opponent_play_order, my_play_order
-    my_moves = ["R"]
-    opponent_history.clear()
-    strategy = [0, 0, 0, 0]
-    opponent_guess = ["", "", "", ""]
-    strategy_guess = ["", "", "", ""]
-    opponent_play_order = {}
-    my_play_order = {}
+  return guess
